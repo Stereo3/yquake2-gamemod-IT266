061117231caf47dd2f6f16af85be774e28726ea0
Russell <rlohinau@gmail.com>
2023-11-14 12:49:56 -0500

    continuing chase cam

--- src/game/S_CAM.c
+++ src/game/S_CAM.c
@@ -77,7 +77,114 @@ void ChasecamTrack (edict_t *ent)
     int distance;
     int tot;
 
-    ent->nextthink = level.time +100;
+    ent->nextthink = level.time + 100;
+    if(ent->owner->waterlevel)
+    {
+        ChasecamRemove (ent, "background");
+        return;
+    }
+
+    AngleVectors(ent->owner->client->v_angle, forward, right, up);
+
+    VectorMA(ent->owner->s.origin, ent->chasedist1, forward, spot2);
+
+    spot2[2] = (spot2[2] + 40.000);
+
+    if(ent->owner->client->v_angle[0] < 0.000)
+    {
+        VectorMA(spot2, -(ent->owner->client->v_angle[0] * 0.6), up, spot2);
+    }else if (ent->owner->client->v_angle[0] > 0.000)
+    {
+        VectorMA(spot2, (ent->owner->client->v_angle[0] * 0.6), up, spot2);
+    }
+    
+    tr = gi.trace (ent->owner->s.origin, NULL, NULL, spot2, ent->owner, false);
+
+    VectorSubtract(tr.endpos, ent->owner->s.origin, spot1);
+
+    ent->chasedist1 = VectorLength(spot1);
+
+    VectorCopy(spot2, spot1);
+    spot1[1] += 32;
+
+    tr = gi.trace (spot2, NULL, NULL, spot1, ent->owner, false);
+
+    if(tr.fraction < 1.000)
+    {
+        VectorCopy(tr.endpos, spot2);
+        spot2[2] -= 32;
+    }
+
+    VectorSubtract(spot2, ent->s.origin, dir);
+    VectorNormalize(dir);
+
+    VectorSubtract(spot2, ent->s.origin, spot1);
+    distance = VectorLength(spot1);
+
+    tr = gi.trace(ent->s.origin, NULL, NULL, spot2, ent->owner, false);
+
+    if(tr.fraction == 1.000)
+    {
+        VectorSubtract(ent->s.origin, ent->owner->s.origin, spot1);
+        VectorNormalize(spot1);
+        VectorCopy(spot1, ent->s.angles);
+
+        tot = (distance * 0.400);
+
+        if (tot > 5.200)
+        {
+            ent->velocity[0] = ((dir[0] * distance) * 5.2);
+            ent->velocity[1] = ((dir[1] * distance) * 5.2);
+            ent->velocity[2] = ((dir[2] * distance) * 5.2);
+        }
+        else
+        {
+            if(tot > 1.000)
+            {
+                ent->velocity[0] = ((dir[0] * distance) * tot);
+                ent->velocity[1] = ((dir[1] * distance) * 5.2);
+                ent->velocity[2] = ((dir[2] * distance) * 5.2);
+            }
+            else
+            {
+                ent->velocity[0] = ((dir[0] * distance) * distance);
+                ent->velocity[1] = ((dir[1] * distance) * distance);
+                ent->velocity[2] = ((dir[2] * distance) * distance);
+            }
+        }
+
+        VectorSubtract(ent->owner->s.origin, ent->s.origin, spot1);
+
+        if(VectorLength(spot1)<20)
+        {
+            ent->velocity[0] *= 2;
+            ent->velocity[1] *= 2;
+            ent->velocity[2] *= 2;
+        }
+    }
+    else
+    {
+        VectorCopy(spot2, ent->s.origin);
+        ent->chasedist1 += 2;
+
+        if(ent->chasedist1 > 60.000)
+        {
+            ent->chasedist1 = 60.000;
+        }
+        
+        if(ent->movedir == ent->s.origin)
+        {
+            if(distance>20)
+            {
+                ent->chasedist2++;
+            }
+            //TODO Finish this function
+        }
+    }
+
+
+
+
 }
 
 //Code from here https://web.archive.org/web/20051227030117/http://www.planetquake.com/qdevels/quake2/9_3_98.html
